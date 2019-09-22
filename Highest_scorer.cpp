bf96e69ff9f56cf79376c3efa060558e59a486ab
Pratyush-Gupta <iec2018065@iiita.ac.in>
2019-09-23 03:41:22 +0530

    CODE_to_MANAGE_GC MYFIRST COMMIT

---
+++ Highest_scorer.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+typedef long long ll;
+
+int main()
+{
+     ll runs,wickets,i,rollno;
+     cout<<"Enter the Rollno runs and wickets\n";
+     //cin>>rollno>>runs>>wickets;
+     return 0;
+}

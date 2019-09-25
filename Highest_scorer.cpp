05cd248da354c884573e7d860c8d82858194e1ec
Pratyush-Gupta <iec2018065@iiita.ac.in>
2019-09-25 20:49:17 +0530

    Added Printf

--- Highest_scorer.cpp
+++ Highest_scorer.cpp
@@ -6,6 +6,7 @@ int main()
 {
      ll runs,wickets,i,rollno;
      cout<<"Enter the Rollno runs and wickets\n";
-     //cin>>rollno>>runs>>wickets;
+     cin>>rollno>>runs>>wickets;
+     cout<<"Roll no "<<rollno<<"runs "<<runs<<"wickets "<<wickets<<endl;
      return 0;
 }

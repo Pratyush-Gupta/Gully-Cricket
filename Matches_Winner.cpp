8a14781c35d37cfb8881b5620ad6589b2a8620bd
Pratyush-Gupta <iec2018065@iiita.ac.in>
2019-09-25 23:31:17 +0530

    Created another file

---
+++ Matches_Winner.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+#include<bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+int main()
+{ 
+  //This file will read the outpur file and tell the winner
+  ll Highest=0,i,j;
+  cout<<"Highest Scorer is\n"<<Highest<<endl;
+}

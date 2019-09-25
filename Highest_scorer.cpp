0b5e21974f6f224dac9ebbb9f1519f995d033094
Pratyush-Gupta <iec2018065@iiita.ac.in>
2019-09-25 21:08:07 +0530

    Added  Array

--- Highest_scorer.cpp
+++ Highest_scorer.cpp
@@ -1,12 +1,26 @@
 #include<iostream>
 using namespace std;
 typedef long long ll;
+struct Array
+{
+     char roll[100];
+     ll runs,wickets;
+};
 
 int main()
 {
-     ll runs,wickets,i,rollno;
-     cout<<"Enter the Rollno runs and wickets\n";
-     cin>>rollno>>runs>>wickets;
-     cout<<"Roll no "<<rollno<<"runs "<<runs<<"wickets "<<wickets<<endl;
+     ll runs,wickets,rollno,i,players;
+     struct Array A[100];
+     cout<<"Enter the number of players\n";
+     cin>>players;
+     for(i=1;i<=players;i++)
+      {     cout<<"Enter the 1:Rollnumber 2:runs and 3:wickets\n";
+            cin>>A->roll>>A->runs>>A->wickets;
+      }
+     for(i=1;i<=players;i++)
+     {
+      cout<<A->roll<<" "<<A->runs<<" "<<A->wickets<<endl;
+     }
+    // cout<<"Roll no "<<rollno<<"runs "<<runs<<"wickets "<<wickets<<endl;
      return 0;
 }

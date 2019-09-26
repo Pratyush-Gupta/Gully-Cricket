1aa5fc302049ae43c26137b4bb94ea2bc2018208
Pratyush-Gupta <iec2018065@iiita.ac.in>
2019-09-26 20:55:58 +0530

    Renamed Winner  to Win

---
+++ delete.cpp
@@ -0,0 +1,42 @@
+#include<iostream>
+#include<string.h>
+#include<map>
+#include<bits/stdc++.h>
+int main()
+{
+    return 0;
+    Real world problem :
+    If ever you have travelled by government bus you would realize that often
+    (Problems)
+     (1) there is mess regarding seat capturing,
+     (2) arguing about monetry fares ,
+     (3) bus conductor has to make tickets of all pasengers, has to maintain count of all pasengers.
+     (4) Government transport sector ( Uttar Pradesh Bus service ) ,has to appoint people to crosscheck the number of passengers present 
+      in bus at various location between distination and source.
+
+     1.Many times what happens if we are not present at bus stops ,buses generally do not stop to take us in
+    (because most of the time they think that the person is a local passengers hardly he will travel 10km, which would occupy 
+     seat for long distance passengers) .This is the reason why bus accumulation of at bus stops occur.Various bus conductor 
+     do not leave for destination until they do not get sufficient passengers ( but they miss various passengers in between ).
+     But if they would know that a passenger is present at particular location he can select so , to maximize the profit as
+     well as passengers can board the bus (who just miss 
+     because of not being at bus stop.)
+     2.Generally at random bus stops  and ask the passengers where he would like to go ,(if he is a local passengers conductor )
+      would surely refuse. We can save the fuel which is spent in stopping and starting the buses.
+      Short distance passengers would also able to recognize the bus for themselves..
+     
+     Benefits:
+     1.This would save time of passengers and maximize the profit.
+     2.Would stop corruption if payment gateway is provided in the app.
+     3.Bus accumulation would be stopped.
+     4.Lots of fuel will saved.
+     5.Buses would become more punctional.
+     6.Buses would become active on passengers.
+     7.Real time traking can help in traffic management.
+     Features:
+     web or app solution could have:
+     1.For passengers: marking where want to board and destination:
+     2.Conductor should be able to count of passengers ,thier location on map.
+     3.Confirmatory button for passengers(that he will be getting the seat and we will allowed in bus). 
+
+}

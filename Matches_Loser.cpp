5d27815a8164d094472c8bcab6a7f997412b4cde
Priyesh <ekthapriyesh24@gmail.com>
2019-10-10 15:48:38 +0530

    Added Loser Deatail

---
+++ Matches_Loser.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+#include<bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+int main()
+{ 
+  //This file will read the outpur file and tell the loser
+  ll Lowest=0,i,j;
+  cout<<"Lowest Scorer is\n"<<Lowest<<endl;
+}
